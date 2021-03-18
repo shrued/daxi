66ab9e56677e4d1ca6fb080c80724b845219f065
shrued <shrutisara10@gmail.com>
2021-03-19 01:52:01 +0530

    Moodbuttons

--- src/components/moodboard/index.js
+++ src/components/moodboard/index.js
@@ -1,7 +1,14 @@
 import moment from "moment";
 import React, { useState } from "react";
 import Calendar from "react-calendar";
-import { Container } from "./moodboard";
+import {
+  Board,
+  Container,
+  MoodBoardContainer,
+  MoodButtons,
+  Question,
+  QuestionContainer,
+} from "./moodboard";
 
 export default function MoodBoard() {
   const [value, onChange] = useState(new Date());
@@ -16,40 +23,72 @@ export default function MoodBoard() {
   return (
     <>
       <Container>
-        <Calendar
-          style={{ height: 500 }}
-          onChange={onChange}
-          value={value}
-          tileClassName={({ date, view }) => {
-            if (sad.find((x) => x === moment(date).format("DD-MM-YYYY"))) {
-              return "highlight-sad";
-            } else if (
-              angry.find((x) => x === moment(date).format("DD-MM-YYYY"))
-            ) {
-              return "highlight-angry";
-            } else if (
-              optimistic.find((x) => x === moment(date).format("DD-MM-YYYY"))
-            ) {
-              return "highlight-optimistic";
-            } else if (
-              bored.find((x) => x === moment(date).format("DD-MM-YYYY"))
-            ) {
-              return "highlight-bored";
-            } else if (
-              excited.find((x) => x === moment(date).format("DD-MM-YYYY"))
-            ) {
-              return "highlight-excited";
-            } else if (
-              peaceful.find((x) => x === moment(date).format("DD-MM-YYYY"))
-            ) {
-              return "highlight-peaceful";
-            } else if (
-              happy.find((x) => x === moment(date).format("DD-MM-YYYY"))
-            ) {
-              return "highlight-happy";
-            }
-          }}
-        ></Calendar>
+        <QuestionContainer>
+          <Question>How are you feeling today?</Question>
+        </QuestionContainer>
+        <MoodBoardContainer>
+          <MoodButtons>
+            <button className="mood-button peaceful">
+              <span>Peaceful</span>
+            </button>
+            <button className="mood-button optimistic">
+              <span>Optimistic</span>
+            </button>
+            <button className="mood-button excited">
+              <span>Excited</span>
+            </button>
+            <button className="mood-button happy">
+              <span>Happy</span>
+            </button>
+            <button className="mood-button sad">
+              <span>Sad</span>
+            </button>
+            <button className="mood-button angry">
+              <span>Angry</span>
+            </button>
+            <button className="mood-button bored">
+              <span>Bored</span>
+            </button>
+          </MoodButtons>
+          <Board>
+            <Calendar
+              style={{ height: 500 }}
+              onChange={onChange}
+              value={value}
+              tileClassName={({ date, view }) => {
+                if (sad.find((x) => x === moment(date).format("DD-MM-YYYY"))) {
+                  return "highlight-sad";
+                } else if (
+                  angry.find((x) => x === moment(date).format("DD-MM-YYYY"))
+                ) {
+                  return "highlight-angry";
+                } else if (
+                  optimistic.find(
+                    (x) => x === moment(date).format("DD-MM-YYYY")
+                  )
+                ) {
+                  return "highlight-optimistic";
+                } else if (
+                  bored.find((x) => x === moment(date).format("DD-MM-YYYY"))
+                ) {
+                  return "highlight-bored";
+                } else if (
+                  excited.find((x) => x === moment(date).format("DD-MM-YYYY"))
+                ) {
+                  return "highlight-excited";
+                } else if (
+                  peaceful.find((x) => x === moment(date).format("DD-MM-YYYY"))
+                ) {
+                  return "highlight-peaceful";
+                } else if (
+                  happy.find((x) => x === moment(date).format("DD-MM-YYYY"))
+                ) {
+                  return "highlight-happy";
+                }
+              }}
+            ></Calendar>
+          </Board>
+        </MoodBoardContainer>
       </Container>
     </>
   );
--- src/components/moodboard/moodboard.js
+++ src/components/moodboard/moodboard.js
@@ -6,3 +6,27 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
+export const QuestionContainer = styled.div`
+  padding-top: 40px;
+`;
+
+export const Question = styled.p`
+  font-size: 30px;
+  color: #181818;
+`;
+
+export const MoodBoardContainer = styled.div`
+  display: flex;
+  padding: 40px;
+`;
+
+export const Board = styled.div`
+  padding: 50px;
+`;
+
+export const MoodButtons = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-basis: 50%;
+`;
