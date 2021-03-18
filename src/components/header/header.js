b9e8ce5b00d2d92244898e26482a18ecd1a25969
shrued <shrutisara10@gmail.com>
2021-03-18 19:52:31 +0530

    Header

---
+++ src/components/header/header.js
@@ -0,0 +1,50 @@
+import styled from "styled-components/macro";
+
+export const HeaderContainer = styled.div`
+  background-color: white;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+`;
+
+export const Logo = styled.p`
+  margin: 10px;
+  padding-left: 20px;
+`;
+
+export const NavItem = styled.div`
+  transition: all 500ms ease-in-out;
+  color: black;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 70px;
+  width: 130px;
+  position: relative;
+  margin: 10px;
+
+  &:before {
+    content: "";
+    position: absolute;
+    width: 0;
+    height: 2px;
+    bottom: 0;
+    left: 0;
+    background-color: #cf93c8;
+    visibility: hidden;
+    transition: all 0.3s ease-in-out;
+  }
+
+  &:hover:before {
+    visibility: visible;
+    width: 100%;
+  }
+
+  @media (max-width: 900px) {
+    display: flex;
+    justify-content: left;
+    padding-left: 10px;
+    width: 100%;
+  }
+`;
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,50 @@
 import React from "react";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+
+import { HeaderContainer, Logo, NavItem } from "./header";
+// import logo from "../../images/logo-light.png";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <HeaderContainer>
+        <Navbar collapseOnSelect expand="lg" className="p-0">
+          <Navbar.Brand href="/">
+            <Logo>Daxi</Logo>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="p-0 ml-auto">
+              <Nav.Link href="/" className="p-0">
+                <NavItem>Home</NavItem>
+              </Nav.Link>
+              <NavItem>
+                <NavDropdown title="Features" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="/articles">Articles</NavDropdown.Item>
+                  <NavDropdown.Item href="/moodboard">
+                    Mood Tracking Board
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/moodpath">
+                    Mood Path
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/reflection">
+                    Reflection
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </NavItem>
+              <Nav.Link href="/therapy" className="p-0">
+                <NavItem>Therapy</NavItem>
+              </Nav.Link>
+              <Nav.Link href="/help" className="p-0">
+                <NavItem>Help</NavItem>
+              </Nav.Link>
+              <Nav.Link href="/about" className="p-0">
+                <NavItem>About</NavItem>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </HeaderContainer>
+    </>
+  );
 }
