fe24a340516131477c4935a76d11c4233c5ac63e
shrued <shrutisara10@gmail.com>
2021-03-18 21:45:02 +0530

    Homepage cloudwave

---
+++ src/components/home/home.js
@@ -0,0 +1,17 @@
+import styled from "styled-components/macro";
+
+export const CloudWave = styled.div`
+  position: relative;
+  height: 100%;
+  background-image: url("https://images.unsplash.com/photo-1495954484750-af469f2f9be5?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ");
+`;
+
+export const WaveContent = styled.div`
+  height: 600px;
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  padding-left: 40px;
+  color: white;
+  font-size: 3.76rem;
+`;
