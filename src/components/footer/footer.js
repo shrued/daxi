b862bb99b645dbe1a04535b0fd136356e7831fca
shrued <shrutisara10@gmail.com>
2021-03-18 23:21:11 +0530

    Footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,15 +2,15 @@ import styled from "styled-components/macro";
 
 export const Container = styled.div`
   display: flex;
-  justify-content: flex-end;
-  color: black;
-  font-size: 20px;
-  padding: 5px 0;
-  background-color: #996d94;
+  justify-content: space-evenly;
+  color: white;
+  font-size: 16px;
+  padding: 20px;
+  background-color: #181818;
 `;
 
 export const Text = styled.div`
-  font-size: 13px;
+  font-size: 16px;
   padding-top: 16px;
   padding-right: 6px;
 `;
@@ -26,4 +26,35 @@ export const Icon = styled.a`
     text-decoration: none;
     color: inherit;
   }
+`;
+
+export const Tabs = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-left: 20px;
+`;
+
+export const TabHeading = styled.p`
+  font-size: 24px;
+  color: white;
+`;
+
+export const TabItem = styled.a`
+  font-size: 20px;
+  color: #555;
+  text-decoration: none;
+
+  &:hover,
+  &:focus,
+  &:active {
+    text-decoration: none;
+    color: inherit;
+    cursor: pointer;
+  }
+`;
+
+export const Info = styled.div`
+  font-size: 24px;
 `;
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Container, Icon, Text } from "./footer";
+import {
+  Container,
+  Icon,
+  Info,
+  TabHeading,
+  TabItem,
+  Tabs,
+  Text,
+} from "./footer";
 
 import {
   MediumOutlined,
@@ -14,25 +22,39 @@ export default function Footer() {
   return (
     <>
       <Container>
-        <Text>Copyright © 2021 Daxi. All Rights Reserved.</Text>
-        <Icon href="#" target="_blank" rel="noreferrer">
-          <FacebookOutlined />
-        </Icon>
-        <Icon href="#" target="_blank" rel="noreferrer">
-          <InstagramOutlined />
-        </Icon>
-        <Icon href="#" target="_blank" rel="noreferrer">
-          <MediumOutlined />
-        </Icon>
-        <Icon href="#" target="_blank" rel="noreferrer">
-          <LinkedinOutlined />
-        </Icon>
-        <Icon href="#" target="_blank" rel="noreferrer">
-          <TwitterOutlined />
-        </Icon>
-        <Icon href="#" target="_blank" rel="noreferrer">
-          <YoutubeOutlined />
-        </Icon>
+        <Tabs>
+          <TabHeading>Company</TabHeading>
+          <TabItem href="/about">About Us</TabItem>
+          <TabItem href="/help">Help</TabItem>
+        </Tabs>
+        <Tabs>
+          <TabHeading>Features</TabHeading>
+          <TabItem href="/articles">Articles</TabItem>
+          <TabItem href="/moodboard">Mood Tracking Board</TabItem>
+          <TabItem href="/moodpath">Mood Path</TabItem>
+          <TabItem href="/reflection">Reflection</TabItem>
+        </Tabs>
+        <Info>
+          <Text>Copyright © 2021 Daxi. All Rights Reserved.</Text>
+          <Icon href="#" target="_blank" rel="noreferrer">
+            <FacebookOutlined />
+          </Icon>
+          <Icon href="#" target="_blank" rel="noreferrer">
+            <InstagramOutlined />
+          </Icon>
+          <Icon href="#" target="_blank" rel="noreferrer">
+            <MediumOutlined />
+          </Icon>
+          <Icon href="#" target="_blank" rel="noreferrer">
+            <LinkedinOutlined />
+          </Icon>
+          <Icon href="#" target="_blank" rel="noreferrer">
+            <TwitterOutlined />
+          </Icon>
+          <Icon href="#" target="_blank" rel="noreferrer">
+            <YoutubeOutlined />
+          </Icon>
+        </Info>
       </Container>
     </>
   );
