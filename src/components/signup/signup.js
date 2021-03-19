50957247fdff619a05ab89e54e0a3f16dbcbcdb3
shrued <shrutisara10@gmail.com>
2021-03-19 13:43:53 +0530

    signup outlook temp

---
+++ src/components/signup/signup.js
@@ -0,0 +1,20 @@
+import styled from "styled-components/macro";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SubContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
