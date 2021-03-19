9cac3bf6ed73827f497d4b59ce49f8fc09ac99cb
shrued <shrutisara10@gmail.com>
2021-03-19 13:40:58 +0530

    sign in outlook temp

--- src/components/signin/index.js
+++ src/components/signin/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Container, FormContainer, SubContainer } from "./signin";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
@@ -19,33 +20,23 @@ const SignIn = () => {
   };
 
   return (
-    <div className="mt-8">
-      <h1 className="text-3xl mb-2 text-center font-bold">Sign In</h1>
-      <div className="border border-blue-400 mx-auto w-11/12 md:w-2/4 rounded py-8 px-4 md:px-8">
-        {error !== null && (
-          <div className="py-4 bg-red-600 w-full text-white text-center mb-3">
-            {error}
-          </div>
-        )}
-        <form className="">
-          <label htmlFor="userEmail" className="block">
-            Email:
-          </label>
+    <Container>
+      <h1>Sign In</h1>
+      <SubContainer>
+        {error !== null && <div>{error}</div>}
+        <FormContainer>
+          <label htmlFor="userEmail">Email:</label>
           <input
             type="email"
-            className="my-1 p-1 w-full"
             name="userEmail"
             value={email}
             placeholder="E.g: faruq123@gmail.com"
             id="userEmail"
             onChange={(event) => onChangeHandler(event)}
           />
-          <label htmlFor="userPassword" className="block">
-            Password:
-          </label>
+          <label htmlFor="userPassword">Password:</label>
           <input
             type="password"
-            className="mt-1 mb-3 p-1 w-full"
             name="userPassword"
             value={password}
             placeholder="Your Password"
@@ -53,24 +44,17 @@ const SignIn = () => {
             onChange={(event) => onChangeHandler(event)}
           />
           <button
-            className="bg-green-400 hover:bg-green-500 w-full py-2 text-white"
             onClick={(event) => {
               signInWithEmailAndPasswordHandler(event, email, password);
             }}
           >
             Sign in
           </button>
-        </form>
-        <p className="text-center my-3">or</p>
-        <button className="bg-red-500 hover:bg-red-600 w-full py-2 text-white">
-          Sign in with Google
-        </button>
-        <p className="text-center my-3">
-          Don't have an account?{" "}
-          <a href="/signup" className="text-blue-500 hover:text-blue-600">
-            Sign up here
-          </a>{" "}
-          <br />{" "}
+        </FormContainer>
+        <p>or</p>
+        <button>Sign in with Google</button>
+        <p>
+          Don't have an account? <a href="/signup">Sign up here</a> <br />{" "}
           {/* <a
             href="/passwordReset"
             className="text-blue-500 hover:text-blue-600"
@@ -78,8 +62,8 @@ const SignIn = () => {
             Forgot Password?
           </a> */}
         </p>
-      </div>
-    </div>
+      </SubContainer>
+    </Container>
   );
 };
 export default SignIn;
