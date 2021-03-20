7aba16eeee3e53f66ee41d1c0f088c551c712219
shrued <shrutisara10@gmail.com>
2021-03-20 17:08:25 +0530

    Articles from temp api

--- src/components/articles/index.js
+++ src/components/articles/index.js
@@ -1,11 +1,46 @@
 import React from "react";
 import { Container, TextContainer } from "./articles";
 
-export default function Articles() {
-  return (
-    <Container>
-      <TextContainer>Articles page</TextContainer>
-      <TextContainer>Read some self-help articles here.</TextContainer>
-    </Container>
-  );
+class Articles extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.mocki.io/v1/3f47888a")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        });
+      });
+  }
+
+  render() {
+    var { isLoaded, items } = this.state;
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <p>Articles</p>
+          <ul>
+            {items.map((item) => (
+              <li key={item.id}>
+                Title: {item.title} | Author: {item.author} <br />
+                {item.article}
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  }
 }
+
+export default Articles;
