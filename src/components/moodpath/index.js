335f6caa026685040ed131930d2b28750279669b
shrued <shrutisara10@gmail.com>
2021-03-20 20:50:02 +0530

    questions in moodpath

--- src/components/moodpath/index.js
+++ src/components/moodpath/index.js
@@ -1,5 +1,87 @@
 import React from "react";
+import { Form, Radio, Slider } from "antd";
+import { Container, SubTitle, Title } from "./moodpath";
 
 export default function MoodPath() {
-  return <div>MoodPath page</div>;
+  return (
+    <Container>
+      <Title>MoodPath page</Title>
+      <SubTitle>
+        You will be asked to answer a bunch of questions about your emotional
+        and physical well-being. These questions will help you reflect on your
+        daily experiences and moods.
+      </SubTitle>
+      <Form>
+        <Form.Item
+          name="radio-button"
+          label="Do you often experience extreme inner turmoil that has no obvious cause?"
+        >
+          <Radio.Group>
+            <Radio.Button value="a">Yes</Radio.Button>
+            <Radio.Button value="b">No</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item name="radio-button" label="Did you sleep poorly last night?">
+          <Radio.Group>
+            <Radio.Button value="a">Yes</Radio.Button>
+            <Radio.Button value="b">No</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item name="radio-button" label="Are you feeling down or sad?">
+          <Radio.Group>
+            <Radio.Button value="a">Yes</Radio.Button>
+            <Radio.Button value="b">No</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item
+          name="radio-button"
+          label="Have you recently talked openly about your feelings?"
+        >
+          <Radio.Group>
+            <Radio.Button value="a">Yes</Radio.Button>
+            <Radio.Button value="b">No</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item
+          name="radio-button"
+          label="Do you often spend a lot of time trying to perfect things that you have already done well?"
+        >
+          <Radio.Group>
+            <Radio.Button value="a">Yes</Radio.Button>
+            <Radio.Button value="b">No</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item
+          name="radio-button"
+          label="Do you use your smartphone to escape from negative feelings?"
+        >
+          <Radio.Group>
+            <Radio.Button value="a">Yes</Radio.Button>
+            <Radio.Button value="b">No</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item
+          name="radio-button"
+          label="Do you have persistent or recurring pain?"
+        >
+          <Radio.Group>
+            <Radio.Button value="a">Yes</Radio.Button>
+            <Radio.Button value="b">No</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item name="slider" label="How do you feel right now?">
+          <Slider
+            marks={{
+              0: "Very bad",
+              20: "Bad",
+              40: "Moderate",
+              60: "Good",
+              80: "Very good",
+              100: "Not sure",
+            }}
+          />
+        </Form.Item>
+      </Form>
+    </Container>
+  );
 }
