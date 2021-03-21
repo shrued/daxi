735acffe76d72b74b5a7854ce64b618e80f2c4e9
shrued <shrutisara10@gmail.com>
2021-03-21 10:43:52 +0530

    Global font

--- src/global-styles.js
+++ src/global-styles.js
@@ -21,7 +21,7 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
   html, body {
-  font-family: sans-serif;
+  font-family: "Montserrat", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   background-color: white;
